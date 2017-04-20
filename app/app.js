92f68cb5c3762b2ca3d65dfa9122a17b6813784b
Gabriel West <gabe_west1@yahoo.com>
2017-04-20 17:10:54 -0500

    implemented react-router-dom

--- app/app.js
+++ app/app.js
@@ -1,6 +1,11 @@
 import React from "react"
 import { render } from "react-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import App from "containers/app/"
 
 
-render(<App />, document.getElementById("app"))
+render(
+    <Router>
+        <App />
+    </Router>
+, document.getElementById("app"))
