8aa6f183d10c7fe63b1cb43c9577f7c0a8d4861a
Gabriel West <gabe_west1@yahoo.com>
2017-04-21 04:37:14 -0500

    Added redux devtools debug extension to store

--- app/app.js
+++ app/app.js
@@ -21,9 +21,10 @@ const middleware = routerMiddleware(history)
 const store = createStore(
   combineReducers({
     ...reducers,
-    router: routerReducer
+    router: routerReducer,
   }),
-  applyMiddleware(middleware)
+  applyMiddleware(middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 ReactDOM.render(
