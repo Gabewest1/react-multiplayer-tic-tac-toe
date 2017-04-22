e6474b12accba69c260b6f14521096cca68974aa
Gabriel West <gabe_west1@yahoo.com>
2017-04-21 21:54:01 -0500

    Updated inital reducers setup

---
+++ app/containers/MatchMakingPage/reducer.js
@@ -0,0 +1,20 @@
+import {
+    FOUND_OPPONENT,
+    ERROR
+} from "./constants"
+
+let initialState = {
+    foundOpponent: false,
+    error: false
+}
+
+export default function matchMakingReducer(state = initialState, action) {
+    switch(action.state) {
+        case FOUND_OPPONENT:
+            return {...state, foundOpponent: true}
+        case ERROR:
+         return {...state, error: true}
+        default:
+            return state
+    }
+}
--- app/reducers.js
+++ app/reducers.js
@@ -1,3 +1,2 @@
-export default () => {
-    return {Hey: 12}
-}
+import HomePage from "containers/HomePage/reducer"
+import MatchMakingPage from "containers/MatchMakingPage/reducer"
