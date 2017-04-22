56ddac1f0ce2e832d5780a163a9d66333235fd30
Gabriel West <gabe_west1@yahoo.com>
2017-04-21 23:02:19 -0500

    Decoupled the Row and Tile components from the TicTacToeBoard component and assembled them all in the TicTacToe container

--- app/components/TicTacToeBoard/index.js
+++ app/components/TicTacToeBoard/index.js
@@ -1,34 +1,16 @@
 import React from "react"
 import styled from "styled-components"
 import board from "./board.png"
-import Tile from "components/TicTacToeTile"
 
 let Board = styled.div`
     background-image: url(${board});
     background-size: cover;
     display: inline-block;
 `
-let Row = styled.div`
-    display: block;
-`
 export default (props) => {
     return (
         <Board>
-            <Row>
-                <Tile></Tile>
-                <Tile></Tile>
-                <Tile></Tile>
-            </Row>
-            <Row>
-                <Tile></Tile>
-                <Tile></Tile>
-                <Tile></Tile>
-            </Row>
-            <Row>
-                <Tile></Tile>
-                <Tile></Tile>
-                <Tile></Tile>
-            </Row>
+            {props.children}
         </Board>
     )
 }
--- app/containers/TicTacToe/index.js
+++ app/containers/TicTacToe/index.js
@@ -1,12 +1,30 @@
 import React from "react"
 import TicTacToeBoard from "components/TicTacToeBoard"
+import Tile from "components/TicTacToeTile"
 import Wrapper from "./Wrapper"
+import Row from "./Row"
 
 export default class TicTacToe extends React.Component {
     render() {
         return (
             <Wrapper>
-                <TicTacToeBoard />
+                <TicTacToeBoard>
+                    <Row>
+                        <Tile></Tile>
+                        <Tile></Tile>
+                        <Tile></Tile>
+                    </Row>
+                    <Row>
+                        <Tile></Tile>
+                        <Tile></Tile>
+                        <Tile></Tile>
+                    </Row>
+                    <Row>
+                        <Tile></Tile>
+                        <Tile></Tile>
+                        <Tile></Tile>
+                    </Row>
+                </TicTacToeBoard>
             </Wrapper>
         )
     }
