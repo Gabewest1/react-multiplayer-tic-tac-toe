daa70361080cf5f33a024f98b66ab6bc682b7e00
Gabriel West <gabe_west1@yahoo.com>
2017-04-25 14:32:34 -0500

    Created a component to display animated text while searching for an online match

---
+++ app/components/MatchmakingSearchText/Wrapper.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export default styled.div`
+    color: white;
+    font-size: 60px;
+    font-family: Chalk;
+    text-align: center;
+
+    @media (max-width: 510px) {
+        font-size: 44px;
+    }
+`
---
+++ app/components/MatchmakingSearchText/index.js
@@ -0,0 +1,37 @@
+import React from "react"
+import AnimatedSpan from "./AnimatedSpan"
+import Wrapper from "./Wrapper"
+
+export default class MatchmakingSearchText extends React.Component {
+    handleClick(e) {
+        let elements = document.getElementsByClassName("loading")
+        console.log(elements)
+        for(var i=0; i<elements.length; i++) {
+            let elem = elements[i]
+            elem.style["animation-name"] = "loading"
+        }
+    }
+    animateText(text) {
+        let keyCounter = 0
+        text = text.split("").map(letter => { 
+            if(letter !== ".") 
+                return letter
+
+            let $animatedSpan = (
+                <AnimatedSpan key={keyCounter++}>{letter}</AnimatedSpan>
+            )
+            return $animatedSpan
+        })
+        console.log(text)
+        return text
+    }
+    render() {
+        let animatedText = this.animateText(this.props.children)
+        return (
+            <Wrapper>
+                {animatedText}
+            </Wrapper>
+        )
+    }
+}
+
