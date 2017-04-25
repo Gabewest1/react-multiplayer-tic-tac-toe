c37f68e360c6abf0eab8f269680721a1d305529d
Gabriel West <gabe_west1@yahoo.com>
2017-04-25 14:33:48 -0500

    Updated the capitalization of the my reducers names

--- app/reducers.js
+++ app/reducers.js
@@ -1,9 +1,9 @@
-import HomePage from "containers/HomePage/reducer"
-import MatchMakingPage from "containers/MatchMakingPage/reducer"
-import TicTacToe from "containers/TicTacToe/reducer"
+import homePage from "containers/HomePage/reducer"
+import matchMaking from "containers/MatchMakingPage/reducer"
+import ticTacToe from "containers/TicTacToe/reducer"
 
 export default {
-    HomePage,
-    MatchMakingPage,
-    TicTacToe
+    homePage,
+    matchMaking,
+    ticTacToe
 }
