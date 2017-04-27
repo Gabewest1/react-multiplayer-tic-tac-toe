a648fb9de667dbb855e9718d98b21f71b06bfc1c
Gabriel West <gabe_west1@yahoo.com>
2017-04-26 20:51:48 -0500

    Made `setTile` able to recieve a 3rd arg to skip sending an event to the server in the case of playing a local match

--- app/containers/TicTacToe/actions.js
+++ app/containers/TicTacToe/actions.js
@@ -2,10 +2,32 @@ import {
     SET_TILE
 } from "./constants"
 
-export function setTile(tile, team) {
+export function setTile(tile, team, fromClient) {
+    let action;
+    if(fromClient) {
+        action = {
+            type: SET_TILE,
+            tile,
+            team
+        }
+    } else {
+        action = {
+            type: "server/SET_TILE",
+            tile,
+            team
+        }
+    }
+    return action
+}
+
+export function resetGame() {
     return {
-        type: SET_TILE,
-        tile,
-        team
+        type: "server/RESET_TIC_TAC_TOE"
+    }
+}
+
+export function endTurn() {
+    return {
+        type: "server/SET_CURRENT_PLAYER"
     }
 }
