4db897ce16a801bde821baa71aee669258e48e23
Gabriel West <gabe_west1@yahoo.com>
2017-04-26 21:08:40 -0500

    Moved `Button` component from `HomePage` container to `components` directory

---
+++ app/containers/TicTacToe/Button.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+let Button = styled.button `
+    background-color: transparent;
+    color: white;
+    width: 180px;
+    height: 85px;
+    border-radius: 10px;
+    border: solid 8px white;
+    margin: 50px
+    font-size: 22px;
+`
+
+export default Button
