3af3ba6db672963a694c4caa5bce24c9a301c47c
Gabriel West <gabe_west1@yahoo.com>
2017-05-03 15:16:27 -0500

    Changed route from `online` => `rockPaperScissors`

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,7 +7,7 @@ export default class HomePage extends React.Component {
     render() {
         return (
             <Wrapper>
-               <Button to="/local">Play Local</Button>
+               <Button to="/rockPaperScissors">Play Local</Button>
                <Button to="/online">Play Online</Button>
             </Wrapper>
         )
