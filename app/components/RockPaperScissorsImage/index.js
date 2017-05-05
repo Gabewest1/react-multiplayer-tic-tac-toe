8e44b8a74441a553a17e5e652f309744f3a66cf4
Gabriel West <gabe_west1@yahoo.com>
2017-05-05 14:29:58 -0500

    Edited RockPaperScissorsImage component to handle not receiving a prop and displaying a question mark

--- app/components/RockPaperScissorsImage/index.js
+++ app/components/RockPaperScissorsImage/index.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import Rock from "assets/images/rock.png"
 import Paper from "assets/images/paper.png"
 import Scissors from "assets/images/scissors.png"
+import QuestionMark from "assets/images/questionMark.png"
 
 let RockPaperScissorsImage = styled.img`
     max-width: 100%;
@@ -15,11 +16,12 @@ export default (props) => {
     let choices = {
         "rock": Rock,
         "paper": Paper,
-        "scissors": Scissors
+        "scissors": Scissors,
+        "none": QuestionMark
     }
-    let type = props.type.toLowerCase()
+    let type = props.type ? props.type.toLowerCase() : "none"
     let imgSrc = choices[type]
-    console.log("inside rps image", props)
+
     return (
         <RockPaperScissorsImage src={imgSrc} alt={type} />
     )
