af23b16041bb49cd5542e813f959d01df1ed9929
Gabriel West <gabe_west1@yahoo.com>
2017-06-30 15:44:02 -0500

    Added logic to the `rockPaperScissors` action to handle offline play
    The file imports a utils function to generate a move for the computer and to determine the winner of a rock paper scissors match. Uses these functions to determine if the winner of rock paper scissors was the human or computer

--- app/containers/RockPaperScissors/actions.js
+++ app/containers/RockPaperScissors/actions.js
@@ -1,17 +1,44 @@
+import getRockPaperScissorsMove from "utils/getRockPaperScissorsMove"
+import rockPaperScissorsWinner from "utils/rockPaperScissorsWinner"
+
 export function setPlayer(player, team, isPlayersTurn) {
-    return (dispatch) => {
-        dispatch({
-            type: "server/SET_PLAYER",
-            player,
-            team,
-            isPlayersTurn
-        })
-        setTimeout(() => {
+    return (dispatch, getState) => {
+        let { isOnlineMatch } = getState().matchMaking
+
+        //If the match is local, then set the computers to the opposite of 
+        //the players values 
+        if(!isOnlineMatch) {
+            dispatch({
+                type: "SET_PLAYER",
+                player: player === "player1" ? "player2" : "player1",
+                name: "The Ticker",
+                team: team === "x" ? "o" : "x",
+                isPlayersTurn: !isPlayersTurn
+            })
+            dispatch({
+                type: "SET_PLAYER",
+                player,
+                name: "Human",
+                team,
+                isPlayersTurn
+            })
             dispatch({
                 type: "SET_USERS_PLAYER",
                 usersPlayer: player
             })
-        },2000)
+        } else {
+            dispatch({
+                type: "server/SET_PLAYER",
+                player,
+                team,
+                isPlayersTurn
+            })
+            setTimeout(() => dispatch({
+                type: "SET_USERS_PLAYER",
+                usersPlayer: player
+            }), 1000)
+        }
+
     }
 }
 
@@ -24,7 +51,7 @@ export function handleWin() {
 
 export function rockPaperScissors(choice) {
     return (dispatch, getState) => {
-        let isOnlineMatch = getState().matchMaking
+        let { isOnlineMatch } = getState().matchMaking
         let type = isOnlineMatch ? "server/ROCK_PAPER_SCISSORS_MOVE" : "ROCK_PAPER_SCISSORS_MOVE"
         
         dispatch({
@@ -35,11 +62,40 @@ export function rockPaperScissors(choice) {
             type: "SET_USERS_SELECTION",
             payload: choice
         })
+
         if(isOnlineMatch) {
             dispatch({
                 type: "server/SET_OPPONENTS_SELECTION",
                 payload: choice
             })
+        } else {
+            let computersRockPaperScissorsMove = getRockPaperScissorsMove()
+            let winner = rockPaperScissorsWinner(choice, computersRockPaperScissorsMove)
+            dispatch({
+                type: "SET_OPPONENTS_SELECTION",
+                payload: computersRockPaperScissorsMove
+            })
+
+            if(winner === "p1") {
+                dispatch({
+                    type: "ROCK_PAPER_SCISSORS_WINNER",
+                    winner: "YOU"
+                })
+                dispatch({
+                    type: "ROCK_PAPER_SCISSORS_WON",
+                })
+            } else if(winner === "p2") {
+                dispatch({
+                    type: "ROCK_PAPER_SCISSORS_WINNER",
+                    winner: "COMPUTER"
+                })
+                dispatch({
+                    type: "ROCK_PAPER_SCISSORS_LOSS",
+                })
+            } else {
+                dispatch(resetRockPaperScissors())
+            }
+
         }
     }
 }
