28e378c306d40e01fac4a153fd8b85f5e5cef901
Gabriel West <gabe_west1@yahoo.com>
2017-07-29 20:07:24 -0500

    Removed flex-grow to stop the element from expanding past its default size

--- app/containers/RockPaperScissors/RockPaperScissorsSelections.js
+++ app/containers/RockPaperScissors/RockPaperScissorsSelections.js
@@ -2,9 +2,8 @@ import styled from "styled-components"
 const RockPaperScissorsSelections = styled.div`
     width: 100%;
     display: flex;
-    justify-content: space-around;
     align-items: center;
-    flex-grow: 1;
+    justify-content: space-around;
 `
 
 export default RockPaperScissorsSelections
