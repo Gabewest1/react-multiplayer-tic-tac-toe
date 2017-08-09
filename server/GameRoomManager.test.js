1b3425d94357cadc171295bf2d7801fc4a7a0e3b
Gabriel West <gabe_west1@yahoo.com>
2017-08-09 15:52:03 -0500

    Updated tests to use the new SocketMock

--- server/GameRoomManager.test.js
+++ server/GameRoomManager.test.js
@@ -23,167 +23,148 @@ describe("GameManager", () => {
         gameRoomManager = new (require("./GameRoomManager"))()
         done()
     })
-    // it("should add a player to a game room", (done) => {
-    //     client.on("connect", () => {
-    //         gameRoomManager.addPlayer(client)
-    //         let playersGameRoom = gameRoomManager.findPlayersGameRoom(client)
-    //         expect(playersGameRoom.players[0]).to.equal(client)
-    //         client.disconnect()
-    //         done()
-    //     })
-    // })
-    // it("should add a spectator to a game room", (done) => {
 
-    //     client.on("connect", () => {
-    //         gameRoomManager.addSpectator(client)
+    it("should add a player to a game room", (done) => {
+        let client = new SocketMock()
+        gameRoomManager.addPlayer(client)
 
-    //         let playersGameRoom = gameRoomManager.findPlayersGameRoom(client)
+        let playersGameRoom = gameRoomManager.findPlayersGameRoom(client)
+        expect(playersGameRoom.players[0]).to.equal(client)
 
-    //         expect(playersGameRoom.spectators[0]).to.equal(client)
-    //         client.disconnect()
-    //         done()
-    //     })
-    // })
-    // it("should have 1 game room setup after adding 1 players", (done) => {
-    //     gameRoomManager.addPlayer(client)
-    //     gameRoomManager.gameRooms.length.should.equal(1)
+        done()
+    })
 
-    //     client.disconnect()
-    //     done()
-    // })
-    // it("should have 1 game room setup after adding 2 players", (done) => {
-    //     let client2 = io(SERVER_URL)
-    //     gameRoomManager.addPlayer(client)
-    //     gameRoomManager.addPlayer(client2)
-    //     gameRoomManager.gameRooms.length.should.equal(1)
+    it("should add a spectator to a game room", (done) => {
+        let client = new SocketMock()
 
-    //     client.disconnect()
-    //     client2.disconnect()
-    //     done()
-    // })
-    // it("should have 2 game rooms setup after adding 3 players", (done) => {
-    //     let client2 = io(SERVER_URL)
-    //     let client3 = io(SERVER_URL)
+        gameRoomManager.addSpectator(client)
 
-    //     gameRoomManager.addPlayer(client)
-    //     gameRoomManager.addPlayer(client2)
-    //     gameRoomManager.addPlayer(client3)
+        let playersGameRoom = gameRoomManager.findPlayersGameRoom(client)
 
-    //     gameRoomManager.gameRooms.length.should.equal(2)
+        expect(playersGameRoom.spectators[0]).to.equal(client)
 
-    //     client.disconnect()
-    //     client2.disconnect()
-    //     client3.disconnect()
-    //     done()
-    // })
-    // it("should have 2 game rooms setup after adding 4 players", (done) => {
-    //     let client2 = io(SERVER_URL)
-    //     let client3 = io(SERVER_URL)
-    //     let client4 = io(SERVER_URL)
+        done()
+    })
+
+    it("should have 1 game room setup after adding 1 players", (done) => {
+        let client = new SocketMock()
+        gameRoomManager.addPlayer(client)
+        gameRoomManager.gameRooms.length.should.equal(1)
+
+        done()
+    })
+
+    it("should have 1 game room setup after adding 2 players", (done) => {
+        let client = new SocketMock()
+        let client2 = new SocketMock()
+        gameRoomManager.addPlayer(client)
+        gameRoomManager.addPlayer(client2)
+        gameRoomManager.gameRooms.length.should.equal(1)
+
+        done()
+    })
+
+    it("should have 2 game rooms setup after adding 3 players", (done) => {
+        let client = new SocketMock()
+        let client2 = new SocketMock()
+        let client3 = new SocketMock()
+
+        gameRoomManager.addPlayer(client)
+        gameRoomManager.addPlayer(client2)
+        gameRoomManager.addPlayer(client3)
+
+        gameRoomManager.gameRooms.length.should.equal(2)
+
+        done()
+    })
+
+    it("should have 2 game rooms setup after adding 4 players", (done) => {
+        let client = new SocketMock()
+        let client2 = new SocketMock()
+        let client3 = new SocketMock()
+        let client4 = new SocketMock()
        
-    //     gameRoomManager.addPlayer(client)
-    //     gameRoomManager.addPlayer(client2)
-    //     gameRoomManager.addPlayer(client3)
-    //     gameRoomManager.addPlayer(client4)
+        gameRoomManager.addPlayer(client)
+        gameRoomManager.addPlayer(client2)
+        gameRoomManager.addPlayer(client3)
+        gameRoomManager.addPlayer(client4)
 
-    //     gameRoomManager.gameRooms.length.should.equal(2)
+        gameRoomManager.gameRooms.length.should.equal(2)
 
-    //     client.disconnect()
-    //     client2.disconnect()
-    //     client3.disconnect()
-    //     client4.disconnect()
-    //     done()
-    // }) 
+        done()
+    }) 
+
     it("should alert everyone in a game room when 2 players are found and ready to start the match", (done) => {
-        let client2 = io(SERVER_URL)
-        let clientMock = {emit: (event, action) => {
-            console.log("CALLED EMIT".america, action)
+        let testFn = (action) => {
             expect(action.type).to.equal("FOUND_OPPONENT")
-        }}
-        let client2Mock = {}
-        client2Mock.emit = clientMock.emit
+        }
+        let client = new SocketMock(testFn)
+        let client2 = new SocketMock(testFn)
 
-        client.on("connect", () => {
-            gameRoomManager.addPlayer(clientMock)   
-        })
+        gameRoomManager.addPlayer(client)   
 
-        client2.on("connect", () => {
-            gameRoomManager.addPlayer(client2Mock)  
-        })
+        gameRoomManager.addPlayer(client2)  
 
         setTimeout(() => done(), 1400)
     })
-    // it("should find the game room containing a given player's socket", (done) => {
-    //     let foundGameRoom = false
-    //     client.on("connect", () => {
-    //         gameRoomManager.addPlayer(client)
-    //         let gameRoom = gameRoomManager.findPlayersGameRoom(client)
-            
-    //         if(gameRoom.players.indexOf(client) !== -1) {
-    //             foundGameRoom = true
-    //         } else {
-    //             foundGameRoom = false
-    //         }
 
-    //         foundGameRoom.should.equal(true)
-    //         client.disconnect()
-    //         done()
-    //     })
-    // })
-    // it("should return undefined when looking for a game room of a non-existant player", (done) => {
-    //     client.on("connect", () => {
-    //         let gameRoom = gameRoomManager.findPlayersGameRoom(client)
-    //         expect(gameRoom).equal(undefined)
-            
-    //         client.disconnect()
-    //         done()
-    //     })
-    // })
-    it("should alert players of the rock-paper-scissors results", (done) => {
-        let client2 = io(SERVER_URL)
-        let clientSawMessage = false
-        let client2SawMessage = false
+    it("should find the game room containing a given player's socket", (done) => {
+        let client = new SocketMock()
+        let foundGameRoom = false
 
-        client.on("connect", () => {
-    
-            client.on("action", (winner) => {
-                clientSawMessage = true
-                client.disconnect()
-            })
+        gameRoomManager.addPlayer(client)
+        let gameRoom = gameRoomManager.findPlayersGameRoom(client)
+        
+        if(gameRoom.players.indexOf(client) !== -1) {
+            foundGameRoom = true
+        } else {
+            foundGameRoom = false
+        }
 
-            gameRoomManager.addPlayer(client)
-        })
-        client2.on("connect", () => {
-            client2.on("action", (winner) => {
-                console.log(`Winner is ${winner}`)
-                client2SawMessage = true
-                client2.disconnect()
-            })
+        foundGameRoom.should.equal(true)
 
-            gameRoomManager.addPlayer(client2)
-        })
-
-
-        setTimeout(() => {
-            gameRoomManager.rockPaperScissors(client2, "paper")
-            gameRoomManager.rockPaperScissors(client, "rock")
-            clientSawMessage.should.equal(true)
-            client2SawMessage.should.equal(true)
-            done()
-        }, 6000)
+        done()
     })
-    // it("should receive players RPC move", (done) => {
-    //     let move = "paper"
-    //     client.on("connect", () => {
-    //         gameRoomManager.addPlayer(client)
-    //         gameRoomManager.rockPaperScissors(client, move)
-    //     })
 
-    //     setTimeout(() => {
-    //         let gameRoom = gameRoomManager.findPlayersGameRoom(client)
-    //         expect(gameRoom.rockPaperScissors[0].choice).to.equal(move)
-    //         client.disconnect()
-    //         done()
-    //     }, 3000)
-    // })
+    it("should return undefined when looking for a game room of a non-existant player", (done) => {
+        let client = new SocketMock()
+        let gameRoom = gameRoomManager.findPlayersGameRoom(client)
+        expect(gameRoom).equal(undefined)
+        
+        done()
+    })
+
+    it("should receive players RPC move", (done) => {
+        let client = new SocketMock()
+        let move = "rock"
+
+        gameRoomManager.addPlayer(client)
+
+        let gameRoom = gameRoomManager.findPlayersGameRoom(client)
+
+        expect(gameRoom.rockPaperScissors[0]).to.equal(undefined)
+        gameRoomManager.rockPaperScissors(client, move)
+        expect(gameRoom.rockPaperScissors[0].choice).to.equal(move)
+        
+        done()
+    })
+
+    it("should alert players of the rock-paper-scissors results", (done) => {
+        let testFn = (action) => {
+            if(action.type === "ROCK_PAPER_SCISSORS_WINNER") {
+                expect(action.type).to.equal("ROCK_PAPER_SCISSORS_WINNER")
+                expect(action.winner).to.equal(client2.id)
+            }
+        }
+        let client = new SocketMock(testFn)
+        let client2 = new SocketMock(testFn)
+    
+        gameRoomManager.addPlayer(client)
+        gameRoomManager.addPlayer(client2)
+
+        gameRoomManager.rockPaperScissors(client, "rock")
+        gameRoomManager.rockPaperScissors(client2, "paper")
+
+        setTimeout(() => done(), 1000)
+    })
 })
